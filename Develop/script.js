f6b98d64068576264e18cebb21d303b1816daf32
Bradley Troll <bradleytroll@gmail.com>
2024-01-10 13:02:33 -0500

    Cleans up the code. Removes some notes.

--- Develop/script.js
+++ Develop/script.js
@@ -63,29 +63,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-// my notes:
-
-// window.prompt("sometext", "defaulttext")
-
-// var person = prompt("Please enter your name")
-
-// console.log(person)
-
-// var con = confirm("Do you like milk?")
-// console.log(con)
-
-// above returns true or false
-
-// hint from gabe: loop over an array
-
-// var num = [1, 2]
-// var upper = ["A", "Z"]
-// var special = ["$", "*"]
-// var lower = ["a", "z"]
-
-// var userChoice = []
