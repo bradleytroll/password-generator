2317749e7effcf3049762e089773f7ef9152c454
Bradley Troll <bradleytroll@gmail.com>
2024-01-10 11:06:25 -0500

    Adds arrays of character options for lowercase, uppercase, special characters, and numerals.

--- Develop/script.js
+++ Develop/script.js
@@ -12,6 +12,11 @@ function generatePassword() {
     generatePassword();
   }
 
+var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numericChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "`", "|", "}", "{", "[", "]", ":", ";", "?", ">", "<", ",", ".", "/", "-", "="]
+
 
 }
 
@@ -31,7 +36,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 
 
